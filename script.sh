c46fbd12b1f2e96a54e9e309caeb1c2ba3cb0891
SOUIKI Adam <souiki.adam.sa@gmail.com>
2014-09-18 17:10:21 +0200

    second commit

---
+++ script.sh
@@ -0,0 +1,8 @@
+git config --global user.name "SOUIKI Adam"
+git config --global user.email "souiki.adam.sa@gmail.com"
+touch README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Sweek-Adam/tset.git
+git push -u origin master
